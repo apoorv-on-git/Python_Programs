0e25b514d9d0a59f8d1f2f6d76f6d5e40c2ed54c
Apoorv <apoorv17@navgurukul.org>
2018-01-21 19:32:46 +0530

    Updated slack_bot.py

--- slack_bot.py
+++ slack_bot.py
@@ -3,10 +3,10 @@
 from slackclient import SlackClient
 import time
 
-token = "xoxp-86400529792-267960808803-279459752420-3406cfad8df5b037d0a7fd746e259e94"
+token = ""		#Your auth_token
 sc = SlackClient(token)
-channel = "C2JBSS9ML"
-greeting = "Yesterday's introspection:-\n1. Less productive day.\n2. I feel good.\n3. I enjoyed working with Rishabh Bhaiya.\n4. Yesterday I learned about Tableau and how to create some awesome data visualisations which are interactive too.\n5. I am thankful to the mentors for helping me out and guiding me to the right way of learning.\nToday's agenda:-\nToday, I will learn how to scrape data from a website by creating a python program."
+channel = ""		#Channel ID
+greeting = ""		#Message you want to post
 time.sleep(60*60*7)
 print sc.api_call(
 	"chat.postMessage",
